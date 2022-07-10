fc92cc88eb25f79bd5e8b90dbec84017af40e31f
Ahmed Rafaat <71382395+ahmedrafaat700870@users.noreply.github.com>
2022-07-10 10:25:44 +0200

    configure db-migarte

---
+++ migrations/sqls/20220710081548-Users-up.sql
@@ -0,0 +1,7 @@
+/* Replace with your SQL commands */
+CREATE TABLE users (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  password VARCHAR(250) NOT NULL
+);
